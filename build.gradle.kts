b925a7ece6774086504c8950fcb979ea02699a16
m.s.taranenko <m.s.taranenko@tbank.ru>
2026-03-15 15:36:53 +0300

    initial

---
+++ build.gradle.kts
@@ -0,0 +1,64 @@
+plugins {
+    kotlin("jvm") version "2.1.21"
+    jacoco
+    id("org.jetbrains.kotlinx.kover") version "0.8.3"
+}
+
+group = "tpo.maxim"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("io.mockk:mockk:1.13.8")
+    testImplementation("org.assertj:assertj-core:3.27.7")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.3")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:5.9.3")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.3")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kover {
+    reports {
+        filters {
+            excludes {
+                classes(
+                    "*\$\$inlined\$*",
+                    "*\$lambda\$*",
+                )
+                annotatedBy(
+                    "*Generated*",
+                )
+            }
+        }
+
+        verify {
+            rule {
+                minBound(25)
+            }
+        }
+    }
+}
+
+tasks.register("reports") {
+    group = "reporting"
+    description = "Открывает отчёты в браузере"
+
+    dependsOn(tasks.test, tasks.named("koverHtmlReport"))
+
+    doLast {
+        exec { commandLine("open", "build/reports/tests/test/index.html") }
+        exec { commandLine("open", "build/reports/kover/html/index.html") }
+    }
+}
+
+
+
+
+kotlin {
+    jvmToolchain(21)
+}
---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,5 @@
+package tpo.maxim
+
+fun main() {
+
+}
